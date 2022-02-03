f9b1053b983e50d11e91cf5715f92375ca802e42
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2022-02-03 17:15:18 +0700

    Fixing update cart

--- resources/views/Admin/Keranjang Buku/Modals/update_keranjang.blade.php
+++ resources/views/Admin/Keranjang Buku/Modals/update_keranjang.blade.php
@@ -12,8 +12,29 @@
                     @method('put')
                     @csrf
                     
-                    {{-- Input Id Pembelian --}}
-                    <input type="hidden" name="pembelianId" id="pembelianId" value="{{ $cart->pembelianId }}">    
+                <!-- Awal Input Item -->
+                <div class="row">
+                    <div class="col-md-6 mb-3">
+                        <label for="judul_buku" class="form-label">Judul Buku</label>
+                        <input type="text" class="form-control" id="judul_buku" value="{{  $cart->buku->judul_buku }}" disabled>
+                    </div>
+
+                    {{-- Input Hidden --}}
+                        <input type="hidden" name="harga_buku" id="harga_buku" value="{{ $cart->buku->harga->harga_satuan }}">    
+                        <input type="hidden" name="pembelianId" id="pembelianId" value="{{ $cart->pembelianId }}">    
+                        <input type="hidden" name="bukuId" id="bukuId" value="{{ $cart->bukuId }}">      
+
+                    <div class="col-md-6 mb-3">
+                        <label for="jumlah_beli" class="form-label">Jumlah Beli</label>
+                        <input type="number" name="jumlah_beli" class="form-control @error('jumlah_beli') is-invalid @enderror" id="jumlah_beli" placeholder="1 2 3..." value="{{ old('jumlah_beli', $cart->jumlah_beli) }}">
+                        @error('jumlah_beli')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                </div>
+
             </div>
 
             <div class="modal-footer">
